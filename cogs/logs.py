dc4fb9ae08377d427b28b01c043443a993b00466
aLEGEND21 <arnavmurthi@gmail.com>
2022-06-01 15:28:01 -0400

    Added listeners to log when the bot is added to or removed from a guild

--- cogs/logs.py
+++ cogs/logs.py
@@ -54,10 +54,91 @@ class Logger(commands.Cog):
             """
         )
         em.set_thumbnail(url=ctx.author.avatar.url)
+        em.timestamp = datetime.datetime.now()
         if ctx.command_failed:
             em.color = discord.Color.red()
         else:
             em.color = discord.Color.green()
+
+        # Send the embed to the logging channel
+        channel = self.bot.get_channel(self.bot.log_channels[0])
+        await channel.send(embed=em)
+    
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild: discord.Guild):
+        """Logs when the bot joins a guild.
+        
+        Parameters
+        ----------
+        guild : discord.Guild
+            The guild the bot joined.
+            
+        Returns
+        -------
+        None
+        """
+
+        # Get the guild user count and bot count
+        user_ct = 0
+        bot_ct = 0
+        for m in guild.members:
+            if m.bot:
+                bot_ct += 1
+            else:
+                user_ct += 1
+        
+        # Generate the embed
+        em = discord.Embed(
+            title=f":satellite_orbital: Joined Guild",
+            description=f"""
+            :satellite: Guild Name, ID: `{guild.name}`, `{guild.id}`
+            :busts_in_silhouette: Guild Member Count, User Count, Bot Count: `{guild.member_count}`, `{user_ct}`, `{bot_ct}`
+            :bust_in_silhouette: Guild Owner, ID: `{guild.owner}`, `{guild.owner.id}`
+            """
+        )
+        em.set_thumbnail(url=guild.icon.url)
+        em.color = discord.Color.green()
+        em.timestamp = datetime.datetime.now()
+
+        # Send the embed to the logging channel
+        channel = self.bot.get_channel(self.bot.log_channels[0])
+        await channel.send(embed=em)
+    
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild: discord.Guild):
+        """Logs when the bot is removed from a guild.
+        
+        Parameters
+        ----------
+        guild : discord.Guild
+            The guild the bot was removed from.
+            
+        Returns
+        -------
+        None
+        """
+
+        # Get the guild user count and bot count
+        user_ct = 0
+        bot_ct = 0
+        for m in guild.members:
+            if m.bot:
+                bot_ct += 1
+            else:
+                user_ct += 1
+        
+        # Generate the embed
+        em = discord.Embed(
+            title=f":satellite_orbital: Removed From Guild",
+            description=f"""
+            :satellite: Guild Name, ID: `{guild.name}`, `{guild.id}`
+            :busts_in_silhouette: Guild Member Count, User Count, Bot Count: `{guild.member_count}`, `{user_ct}`, `{bot_ct}`
+            :bust_in_silhouette: Guild Owner, ID: `{guild.owner}`, `{guild.owner.id}`
+            """
+        )
+        em.set_thumbnail(url=guild.icon.url)
+        em.color = discord.Color.red()
+        em.timestamp = datetime.datetime.now()
 
         # Send the embed to the logging channel
         channel = self.bot.get_channel(self.bot.log_channels[0])

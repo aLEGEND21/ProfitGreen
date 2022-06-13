330abc0eb74d7ad18d0d737d5f694516ed24210d
aLEGEND21 <arnavmurthi@gmail.com>
2022-06-13 19:05:54 -0400

    Attempted to fix py-cord being installed incorrectly

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# Make sure py-cord is installed
+import os
+os.system("pip install py-cord==2.0.0rc1")
+
 # Imports
 import discord
 from discord.ext import commands
-
-import os
 
 from config import Config
 from extras import *

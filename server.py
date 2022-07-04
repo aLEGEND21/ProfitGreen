4d41593234fd54dbe8b7ba73295c2d64fec59b10
aLEGEND21 <arnavmurthi@gmail.com>
2022-07-04 12:33:10 -0500

    Updated the server code to also add a reminder to the database for certain users for when they could next vote

--- server.py
+++ server.py
@@ -2,7 +2,17 @@ from pymongo import MongoClient # pymongo is a dependency of motor and will be i
 from flask import Flask
 from threading import Thread
 from config import Config
+from time import time
 from flask import request
+
+# Declare the var to hold all the users to remind them of their next vote
+REMIND_USERS = [
+    #416730155332009984, # aLEGEND#8740
+    388874485303869441, # Brian McLogan#7569
+    726516937676423188, # IamtheBest#7537
+    411315998646468608, # m0m1n#2300
+    516066882256764950  # TheGroke#1123
+]
 
 # Initalize the app
 app = Flask(__name__)
@@ -24,12 +34,19 @@ def topgg_webhook():
     Docs:
     https://docs.top.gg/resources/webhooks/
     """
-    # Add the task to the database
+    # Add the upvote task to the database
     tasks.insert_one({
         "_type": "upvote",
         "website": "top.gg",
         "user": request.json["user"]
     })
+    # Add a task to remind the user to the database
+    if request.json["user"] in REMIND_USERS:
+        tasks.insert_one({
+            "_type": "upvote_reminder",
+            "user": request.json["user"],
+            "remind_timestamp": round(time()) + 60 * 60 * 12 # 12 hours from now
+        })
     return request.json
 
 def run():

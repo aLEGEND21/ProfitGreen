e7b4b77688d14f5360dd35c2f01dbe0c42bb37a9
aLEGEND21 <arnavmurthi@gmail.com>
2022-07-14 18:31:24 +0530

    Paginated the help command output using a dropdown

--- cogs/help.py
+++ cogs/help.py
@@ -32,7 +32,8 @@ class ProfitGreenHelpCommand(commands.HelpCommand):
                 max_len = len(cmd.name)
         max_len += 2
 
-        # Generate the embed description from the mapping object
+        # OLD System - Single embed
+        '''# Generate the embed description from the mapping object
         desc = ""
         indent = "\u200b " * 6
         for cog in mapping:
@@ -63,10 +64,66 @@ class ProfitGreenHelpCommand(commands.HelpCommand):
             description=desc,
             color=self.context.bot.green,
             timestamp=datetime.datetime.now()
-        )
+        )'''
 
-        # Create the view containing buttons for different functions
-        view = discord.ui.View(
+        # Create a new cog object for the commands that aren't under a cog
+        misc_cog = commands.Cog(name="Uncategorized Commands")
+        misc_cog.emoji = "notepad_spiral"
+        mapping[misc_cog] = mapping[None]
+        del mapping[None]
+
+        # Remove cogs with no commands from the mapping
+        _mapping = {}
+        for cog, cmds in mapping.items():
+            if cog.get_commands() == []:
+                continue
+            else:
+                _mapping[cog] = cmds
+        mapping = _mapping
+
+        # Create each embed page
+        pages = [
+            discord.Embed(
+                title=f"ProfitGreen Bot Help",
+                description=f"{self.context.bot._emojis['profitgreen']} ProfitGreen is a Discord bot with a wide array of finance and investing related commands, allowing you to do everything from invest in stocks and cryptos to viewing the sentiment of individual stocks or the entire stock market.\n\n*Use the select menu below to view the commands for a specific category.*",
+                color=self.context.bot.green,
+                timestamp=datetime.datetime.now()
+            )
+        ]
+        indent = "\u200b " * 6
+        for cog, cmds in mapping.items():
+            # Create the embed
+            p = discord.Embed(
+                description=f"**{cog.emoji if getattr(cog, 'emoji', False) else ''} {cog.qualified_name}:**\n",
+                color=self.context.bot.green,
+                timestamp=datetime.datetime.now()
+            )
+            # Set the embed description
+            for c in cmds:
+                # Skip over hidden commands and commands that aren't prefixed commands
+                if not isinstance(c, commands.Command) or c.hidden:
+                    continue
+                # Set the amount of spacing between the command and the short_docs
+                spacing = "." * (max_len - len(c.name)) # Use periods because \u200b has variable width which messes up the spacing
+                # Add the command to the embed description
+                p.description += f"{indent}`{c.name}` `{spacing}` *{c.short_doc}*\n" if c.short_doc != "" else f"{indent}`{c.name}`\n"
+            pages.append(p)
+
+        # Create the callback for the select menu
+        async def select_callback(interaction: discord.Interaction):
+            # Retrieve the selected cog's index
+            selected_category = interaction.data['values'][0]
+            for cog in mapping:
+                if cog.qualified_name == selected_category:
+                    cog_index = list(mapping.keys()).index(cog) + 1 # +1 is to account for the default page
+                    break
+            else:
+                cog_index = 0 # Handle the category being the bot overview page
+            # Update the message
+            await interaction.response.edit_message(embeds=[pages[cog_index]])
+
+        # Create the view containing buttons for different functions and the select menu
+        btns = [
             discord.ui.Button(
                 label="Support",
                 url="https://discord.gg/xBSBYk5Adj"
@@ -79,9 +136,27 @@ class ProfitGreenHelpCommand(commands.HelpCommand):
                 label="Vote",
                 url=f"https://top.gg/bot/{self.context.bot.user.id}/vote"
             )
-        )
+        ]
+        select_options = [
+            discord.SelectOption(
+                label="Overview",
+                #emoji=self.context.bot._emojis['profitgreen']
+            )
+        ]
+        for cog in mapping:
+            if cog.get_commands() == []:
+                continue
+            select_options.append(
+                discord.SelectOption(
+                    label=cog.qualified_name,
+                    #emoji=cog.emoji if getattr(cog, 'emoji', False) else None, # TODO: Figure out why tis doesn't work
+                )
+            )
+        select_menu = discord.ui.Select(options=select_options, placeholder="Select a Category")
+        select_menu.callback = select_callback
+        view = discord.ui.View(select_menu, *btns)
 
-        await self.context.send(embeds=[em], view=view)
+        await self.context.send(embeds=[pages[0]], view=view)
     
     async def send_command_help(self, command: commands.Command):
         # TODO: Create something showing proper command usage from the usage examples
